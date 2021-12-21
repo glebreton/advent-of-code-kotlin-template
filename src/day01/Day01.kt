c3c88c67c6166ee043123cf9352e268d047e8162
Goulven <glebreton@sii.fr>
2021-12-21 17:14:21 +0100

    feat: Day 1 / Part 2

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -3,10 +3,17 @@ package day01
 import readInput
 
 fun main() {
-    val depths = readInput("Day01_part1")
-    val depthIncreased = depths
-        .map { it.toInt() }
+    val depths = readInput("day01/Day01_input").map { it.toInt() }
+
+    val depthIncreasedPart1 = depths
         .windowed(2)
         .count { (first, second) -> first < second }
-    println(depthIncreased)
+    println("Part 1 : $depthIncreasedPart1")
+
+    val depthIncreasedPart2 = depths
+        .windowed( 3 )
+        .map { it.sum() }
+        .windowed(2)
+        .count { (first, second) -> first < second }
+    print("Part 2 : $depthIncreasedPart2")
 }
