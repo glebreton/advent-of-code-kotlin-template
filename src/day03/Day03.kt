2f29d0e56e4385cde259fc03cee275c0f914c1d3
Goulven <glebreton@sii.fr>
2021-12-22 11:46:24 +0100

    feat: Day 3 / Part 2

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -1,24 +1,45 @@
 package day03
 
 import readInput
-import kotlin.math.pow
 
-fun buildDiagnosticReport(file: String, inputSize: Int): Array<Int> {
-    val input = readInput("day03/$file")
+fun buildDiagnosticReport(input: List<String>, inputLength: Int, oxygen: Boolean): Int {
+    val report = Array(inputLength) { 0 }
+    var inputList = input;
+    for (index in 0 until inputLength) {
+        if (inputList.size == 1) {
+            report[index] = inputList[0][index].digitToInt()
+        } else {
+            val countByElements = inputList.map { it.elementAt(index) }.groupingBy { it }.eachCount()
+            if (oxygen) {
+                report[index] = if ((countByElements['1'] ?: 0) >= (countByElements['0'] ?: Int.MIN_VALUE)) {
+                    1
+                } else {
+                    0
+                }
+            } else {
+                report[index] = if ((countByElements['0'] ?: Int.MAX_VALUE) <= (countByElements['1'] ?: 0)) {
+                    0
+                } else {
+                    1
+                }
+            }
 
-    val report = Array(inputSize) { it }
-    for (index in 0 until inputSize) {
-        report[index] = input.map { it.elementAt(index) }.count { it == '1' } / (inputSize / 2)
+            inputList = inputList.filter { it.elementAt(index).digitToInt() == report[index] }
+        }
     }
 
-    return report
+    return report.joinToString("") { it.toString() }.toInt(2)
 }
 
 fun main() {
-//    val report = buildDiagnosticReport("Day03_test", 5)
-    val report = buildDiagnosticReport("Day03_input", 12)
+//    val input = readInput("day03/Day03_test")
+//    val inputLength = 5
 
-    val gammaRate = report.joinToString("") { it.toString() }.toInt(2)
-    val epsilonRate = 2.0.pow(12).toInt() -1 - gammaRate
-    println(gammaRate * epsilonRate)
+    val input = readInput("day03/Day03_input")
+    val inputLength = 12
+
+    val oxygenGeneratorRating = buildDiagnosticReport(input, inputLength, true)
+    val co2ScrubberRating = buildDiagnosticReport(input, inputLength, false)
+
+    println("Life support rating = " + (oxygenGeneratorRating * co2ScrubberRating))
 }
